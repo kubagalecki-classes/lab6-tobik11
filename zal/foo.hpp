e2207d831cea34a5f68eb07e839ba1960e308bce
tobik11 <tobik11@onet.pl>
2020-12-13 12:56:58 +0000

    first_try

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,6 +11,9 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     // Twoja implementacja tutaj
+    for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
+
+  
 
     return ret_v;
 }
