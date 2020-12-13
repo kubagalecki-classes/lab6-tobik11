2aac16d0570d5420005f40109f903ed5175617f5
tobik11 <tobik11@onet.pl>
2020-12-13 13:38:40 +0000

    second

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -6,6 +6,8 @@
 #include <list>
 #include <vector>
 
+using namespace std;
+
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
@@ -13,7 +15,9 @@ std::vector< char > foo(std::list< Human >& people)
     // Twoja implementacja tutaj
     for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
 
-  
+    transform(people.rbegin(), people.rend(), ret_v.begin(), [](Human& person) {
+        return person.isMonster() ? 'y' : 'n';
+    });
 
     return ret_v;
 }
